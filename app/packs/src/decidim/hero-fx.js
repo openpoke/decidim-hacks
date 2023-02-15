078f8101fd151f6a714c8f1a2044783fdcb66eb6
Ivan Vergés <ivan@pokecode.net>
2023-02-15 15:00:42 +0100

    add the coolest fx ever

---
+++ app/packs/src/decidim/hero-fx.js
@@ -0,0 +1,7 @@
+$(function() {
+    $(".hero-heading .rotate").textrotator({
+      animation: "spin", // dissolve, fade, flip, flipUp, flipCube, flipCubeUp, spin.
+      separator: ",",
+      speed: 1000
+    });
+  });
